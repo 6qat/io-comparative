ad6976f128faec60cda5e0e7335eab6efecd4c5a
Guilherme Moreira <ceschiatti@gmail.com>
2020-10-13 22:47:49 -0300

    Add actor (untyped) implementation.

--- src/main/scala/RateLimiterQueue.scala
+++ src/main/scala/RateLimiterQueue.scala
@@ -1,4 +1,8 @@
+import akka.actor.{Actor, ActorLogging}
+
 import scala.collection.immutable.Queue
+import scala.concurrent.Future
+import scala.concurrent.duration._
 
 sealed trait RateLimiterTask[F]
 case class Run[F](run: F) extends RateLimiterTask[F]
@@ -20,11 +24,15 @@ case class RunAfter[F](millis: Long) extends RateLimiterTask[F]
 case class RateLimiterQueue[F](
     maxRuns: Int,
     perMillis: Long,
-    lastTimestamps: Queue[Long], // started computations
-    waiting: Queue[F], // waiting computations
-    scheduled: Boolean
+    lastTimestamps: Queue[Long] = Queue(), // started computations
+    waiting: Queue[F] = Queue(), // waiting computations
+    scheduled: Boolean = false
 ) {
 
+  /** Given the timestamp, obtain a list of task which might include running a
+    * computation or scheduling a `run` invocation in the future, and an updated
+    * queue.
+    */
   def run(now: Long): (List[RateLimiterTask[F]], RateLimiterQueue[F]) = {
     pruneTimestamps(now).doRun(now)
   }
@@ -65,6 +73,59 @@ case class RateLimiterQueue[F](
   private def pruneTimestamps(now: Long): RateLimiterQueue[F] = {
     val threshold = now - perMillis
     copy(lastTimestamps = lastTimestamps.filter(_ >= threshold))
+  }
+
+}
+
+// ====================================================================================================================
+
+private case class LazyFuture(t: () => Future[Unit])
+private case object ScheduledRunQueue
+
+private class RateLimiterActor(maxRuns: Int, per: FiniteDuration)
+    extends Actor
+    with ActorLogging {
+
+  import context.dispatcher
+
+  // mutable actor state: the current rate limiter queue; the queue itself is
+  // immutable, but the reference is mutable and access to it is protected by
+  // the actor. // GUIGA: made it immutable
+  private val queue = RateLimiterQueue[LazyFuture](maxRuns, per.toMillis)
+  override def receive: Receive = onMessage(queue)
+
+  private def onMessage(queue: RateLimiterQueue[LazyFuture]): Receive = {
+    case lf: LazyFuture =>
+      // enqueueing the new computation and checking if any computations can be run
+      context.become(onMessage(queue.enqueue(lf)))
+      runQueue()
+
+    case ScheduledRunQueue =>
+      // clearing the `scheduled` flag, as we are in a scheduled run right now, so it's
+      // possible a new one has to be scheduled
+      context.become(onMessage(queue.notScheduled))
+      runQueue()
+  }
+
+  private def runQueue(): Unit = {
+    val now = System.currentTimeMillis()
+
+    val (tasks, queue2) = queue.run(now)
+    // Updating the mutable reference to store the new queue.
+    context.become(onMessage(queue2))
+    // Each task returned by `queue.run` is turned into a side-effect: either running
+    // the lazy future (which amounts to running the block of code which creates the
+    // future - and hence makes the computation run), or scheduling a `ScheduledRunQueue`
+    // message to be sent to the actor after the given delay.
+    tasks.foreach {
+      case Run(LazyFuture(f)) => f()
+      case RunAfter(millis) =>
+        context.system.scheduler.scheduleOnce(
+          millis.millis,
+          self,
+          ScheduledRunQueue
+        )
+    }
   }
 
 }
