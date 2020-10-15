4533c5f1e2dfb66a5d3ec5df27fdec76b052b784
Guilherme Moreira <ceschiatti@gmail.com>
2020-10-15 13:22:51 -0300

    Playing with fs2

--- src/main/scala/fs2_lab.scala
+++ src/main/scala/fs2_lab.scala
@@ -1,4 +1,4 @@
-class fs2_lab {
+object fs2_lab extends App {
   import cats.effect.IO
   import fs2._
 
@@ -12,19 +12,43 @@ class fs2_lab {
     (Stream(1, 2) ++ Stream(3)
       .map(_ => throw new Exception("nooo!!!"))).attempt.toList
 
-  def tk[F[_], O](n: Long): Pipe[F, O, O] =
-    (in: Stream[F, O]) =>
-      in.scanChunksOpt(n) { l: Long =>
-        if (l <= 0) None
-        else
-          Some(c =>
-            c.size match {
-              case m if m < l => (l - m, c)
-              case m          => (0, c.take(l.toInt))
-            }
-          )
-      }
+  def tk[F[_], O](initialState: Long): Pipe[F, O, O] =
+    (in: Stream[F, O]) => {
 
-  val o4: List[Int] = Stream(1, 2, 3, 4).through(tk(2)).toList
+      val out: Stream[F, O] =
+        in
+          .scanChunksOpt(initialState) { currentState: Long =>
+            if (currentState <= 0) None
+            else
+              Some((c: Chunk[O]) => {
+                val tup: (Long, Chunk[O]) = c.size match {
+                  case m if m < currentState => (currentState - m, c)
+                  case m                     => (0, c.take(currentState.toInt))
+                }
+                tup
+              })
+          }
+
+      out
+
+    }
+
+  def tk2[F[_], O](n: Long): Pipe[F, O, O] = {
+    def go(s: Stream[F, O], n: Long): Pull[F, O, Unit] = {
+      s.pull.uncons.flatMap {
+        case Some((hd, tl)) =>
+          hd.size match {
+            case m if m <= n => Pull.output(hd) >> go(tl, n - m)
+            case m           => Pull.output(hd.take(n.toInt)) >> Pull.done
+          }
+        case None => Pull.done
+      }
+    }
+    in => go(in, n).stream
+  }
+
+  val o4: List[Int] = Stream(1, 2, 3, 4, 5, 6, 7, 8).through(tk(3)).toList
+  println(o4)
+  println(Stream(1, 2, 3, 4, 5, 6, 7, 8).through(tk2(3)).toList)
 
 }
